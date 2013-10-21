9f7f9eb73190dff4fec52aee5af182233b166d98
Alan Said <alan@alans.se>
2013-10-21 17:47:58 +0200

    CookCollector

---
+++ src/main/java/net/recommenders/recipecrawler/PageCrawler.java
@@ -0,0 +1,11 @@
+package net.recommenders.recipecrawler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alan
+ * Date: 2013-10-21
+ * Time: 14:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class PageCrawler {
+}
