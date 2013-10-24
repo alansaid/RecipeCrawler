63e51916bcdc2d06426c99e18c28755062163ae9
Alan Said <alan@alans.se>
2013-10-24 18:09:50 +0200

    ProfileCrawler done

--- src/main/java/net/recommenders/recipecrawler/ProfileCrawler.java
+++ src/main/java/net/recommenders/recipecrawler/ProfileCrawler.java
@@ -7,9 +7,13 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created with IntelliJ IDEA.
@@ -26,25 +30,100 @@ public class ProfileCrawler {
     public static final String reviewURL = "/reviews.aspx?Page=";
     public static final String menuURL = "/menus.aspx?Page=";
     public static final String blogURL = "/blog.aspx?Page=";
+    public static final String profileFile = "profiles.csv";
+    public static final String hasCooksFile = "hasCooks.csv";
+    public static final String hasReviewsFile = "hasReviews.csv";
+    public static final String hasRecipesFile = "hasRecipes.csv";
+    private StringBuffer profiles= new StringBuffer();
+    private StringBuffer recipes = new StringBuffer();
+    private StringBuffer cooks = new StringBuffer();
+    private StringBuffer reviews = new StringBuffer();
 
     public static void main(String[] args) {
 
+        System.out.println("args[0] = fromLine \t (args[1] = toLine)");
+        System.out.println("When no arguments given, all of users.csv is read");
+        int fromLine = 0;
+        int toLine = 0;
+        if(args.length != 0){
+            try{
+                fromLine = Integer.parseInt(args[0]);
+            }catch (NumberFormatException e){
+                logger.info(e.getMessage());
+                System.out.println("Start from line 0");
+            }
+            try{
+                toLine = Integer.parseInt(args[1]);
+            }catch(NumberFormatException e){
+                logger.info(e.getMessage());
+                System.out.println("Read to EOF");
+            }
+            System.out.println("Reading from line " + fromLine + " to line " + toLine);
+        }
         ProfileCrawler pc = new ProfileCrawler();
 
         int userID = 18114940;
         int proUserID = 12256565;
         int inactiveUserID = 188406874;
         int emptyUserID = 13944900;
-        pc.parseProfile(userID);
+//        pc.parseProfile(userID);
+//        pc.parseProfile(proUserID);
+//        pc.parseProfile(inactiveUserID);
+//        pc.parseProfile(emptyUserID);
 
-        pc.parseProfile(proUserID);
-
-        pc.parseProfile(inactiveUserID);
-
-        pc.parseProfile(emptyUserID);
+        pc.parseProfiles(fromLine, toLine);
+//        pc.parseProfile(10000003);
     }
 
-    public boolean parseProfile(int userID){
+    public boolean parseProfiles(int from, int to){
+        int counter = 0;
+        int bufferCounter = 0;
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("users.csv"));
+        } catch (IOException e){
+            logger.error(e.getMessage());
+        }
+
+        while(in.hasNextLine()){
+            if(counter < from){
+                counter++;
+                in.nextLine();
+                continue;
+            }
+            if(to != 0 && counter > to)
+                break;
+            int userID = Integer.parseInt(in.nextLine());
+            parseProfile(userID);
+            if (bufferCounter > 100){
+                writeProfiles(profiles);
+                profiles.setLength(0);
+                writeCooks(cooks);
+                cooks.setLength(0);
+                writeRecipes(recipes);
+                recipes.setLength(0);
+                writeReviews(reviews);
+                reviews.setLength(0);
+                bufferCounter = 0;
+            }
+            System.out.print("\rCrawling line: " + counter + "\t profileID: " + userID);
+            System.out.flush();
+            counter ++;
+            bufferCounter ++;
+        }
+        System.out.println("\n");
+        System.out.println("Crawled: " + (counter - from) + " cooks");
+        if(bufferCounter > 0){
+            writeProfiles(profiles);
+            writeCooks(cooks);
+            writeRecipes(recipes);
+            writeReviews(reviews);
+        }
+        return true;
+    }
+
+
+    public void parseProfile(int userID){
         List<String> userAgents = new ArrayList<String>() {{
             add("Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14");
             add("Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14");
@@ -74,27 +153,31 @@ public class ProfileCrawler {
             add("Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25");
             add("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2");
         }};
-        StringBuffer profiles= new StringBuffer();
         Document doc = null;
+        double randomSleepTime = Math.random() * 5678 + 3876;
 
         try {
+            Thread.sleep((int)randomSleepTime);
             doc = Jsoup.connect(profileURL + userID).userAgent(userAgents.get((int)Math.random()*userAgents.size())).timeout(100000).get();
         }
         catch (IOException e){
             e.printStackTrace();
         }
-
+        catch (InterruptedException ie){
+            logger.error(ie.getMessage());
+            ie.printStackTrace();
+        }
         boolean proUser = !doc.select("a.proPill35x12").isEmpty();
         String name = doc.select("div#ctl00_CenterColumnPlaceHolder_ucProfileDetails_cooksName_litName").text();
         Element homeTownElem = doc.select("div#ctl00_CenterColumnPlaceHolder_ucProfileDetails_ucCooksLocation_HomeTownArea").first();
         String homeTown = homeTownElem.select("span").text();
 
         List<Element> elements = homeTownElem.siblingElements().subList(0,5);
-        String livingIn = elements.get(0).text().split(":")[1].trim();
-        String memberSince= elements.get(1).text().split(":")[1].trim();
-        String cookingLevel = elements.get(2).text().split(":")[1].trim();
-        String cookingInterests = elements.get(3).text().split(":")[1].trim();
-        String hobbies = elements.get(4).text().split(":")[1].trim();
+        String livingIn = (elements.get(0).text()+" ").split(":")[1].trim();
+        String memberSince= (elements.get(1).text()+" ").split(":")[1].trim();
+        String cookingLevel = (elements.get(2).text()+" ").split(":")[1].trim();
+        String cookingInterests = (elements.get(3).text()+" ").split(":")[1].trim();
+        String hobbies = (elements.get(4).text()+" ").split(":")[1].trim();
 
         String user = name + "\t" +
                 homeTown + "\t" +
@@ -109,25 +192,44 @@ public class ProfileCrawler {
         boolean likesCooks = !doc.select("a#ctl00_RightColumnTopPlaceHolder_ucCooksILike_lnkViewAll").isEmpty();
         boolean hasRecipeBox = !doc.select("div#ctl00_CenterColumnPlaceHolder_divSharedRecipeBoxHeader > div.title").first().text().equals("Recipe Box 0 recipes");
         boolean hasReviewBox = !doc.select("div#ctl00_CenterColumnPlaceHolder_ucProfileReviewList_divHeader > div.title").first().text().equals("Recipe Reviews");
-
-//ctl00_CenterColumnPlaceHolder_ucProfileReviewList_divHeader
-
-        System.out.println(user);
-        System.out.println("likesCooks: " + likesCooks);
-        System.out.println("hasRecipes: " + hasRecipeBox);
-        System.out.println("hasReviews: " + hasReviewBox);
-        return true;
+        if(likesCooks)
+            cooks.append(userID+", "+likesCooks + "\n");
+        if(hasRecipeBox)
+            recipes.append(userID + ", " + hasRecipeBox + "\n");
+        if(hasReviewBox)
+            reviews.append(userID + ", " + hasReviewBox + "\n");
+        profiles.append(user + "\n");
+//        System.out.println(user);
+//        System.out.println("likesCooks: " + likesCooks);
+//        System.out.println("hasRecipes: " + hasRecipeBox);
+//        System.out.println("hasReviews: " + hasReviewBox);
     }
 
-    public boolean parseReviews(int userID){
-        return true;
+
+    private boolean writeProfiles(StringBuffer profiles){
+        return writeData(profiles, profileFile);
+    }
+    private boolean writeCooks(StringBuffer cooks){
+        return writeData(cooks, hasCooksFile);
+    }
+    private boolean writeRecipes(StringBuffer recipes){
+        return writeData(recipes, hasRecipesFile);
+    }
+    private boolean writeReviews(StringBuffer reviews){
+        return writeData(reviews, hasReviewsFile);
     }
 
-    public boolean parseRecipes(int userID){
-        return true;
-    }
-
-    public boolean parseCooks(int userID){
+    public boolean writeData(StringBuffer input, String filename){
+        try{
+            BufferedWriter out = new BufferedWriter(new FileWriter(filename, true));
+            out.write(input.toString());
+            out.flush();
+            out.close();
+        }
+        catch (IOException e){
+            logger.error(e.getMessage());
+            return false;
+        }
         return true;
     }
 }
